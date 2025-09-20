16daccfd3fe168e0915d3cb3472d9824fbaae1a8
PasqualeAlexander <alexanderpasquale1@gmail.com>
2025-09-20 14:57:02 -0300

    update

--- bot.js
+++ bot.js
@@ -1604,7 +1604,7 @@ const roomConfig = {
     password: null,
     maxPlayers: 18,
     public: true,  // Cambiar a true para que la sala sea pública
-    token: "thr1.AAAAAGjOvQ8ZaTPCWRKZJA.W6USfsee1dw", // Token actualizado
+    token: "thr1.AAAAAGjO6tLEf9UmXNK-Sg.ETO3ZX0yozg", // Token actualizado
     geo: { code: 'AR', lat: -34.7000, lon: -58.2800 },  // Ajustado para Quilmes, Buenos Aires
     noPlayer: true
 };
